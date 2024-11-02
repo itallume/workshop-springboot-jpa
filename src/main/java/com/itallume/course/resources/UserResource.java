13c1b88f3ed050ed676c9442bf6bc8974c779a19
itallume <itallo.oliver21@gmail.com>
2024-11-02 02:34:22 -0300

    feat: implementação do UserServico

--- src/main/java/com/itallume/course/resources/UserResource.java
+++ src/main/java/com/itallume/course/resources/UserResource.java
@@ -1,17 +1,29 @@
 package com.itallume.course.resources;
 
 import com.itallume.course.entities.User;
+import com.itallume.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping(value = "/users")
 public class UserResource {
+
+    @Autowired
+    private UserService service;
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(1L, "maria", "maria@gmail.com", "999999999", "12345");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
